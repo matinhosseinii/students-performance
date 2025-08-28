bb5a173fd8bec70500b0ed3cf03644c4d346c032
matinhosseinii <matinhosseini8102@gmail.com>
2025-08-28 09:15:44 +0330

    add custom exception

--- src/StudentsPerformance/exception/custom_exception.py
+++ src/StudentsPerformance/exception/custom_exception.py
@@ -1,13 +1,27 @@
-# class CustomException(Exception):
-#     """
-#     Custom exception class for handling specific errors in the project.
-#     """
+import sys
 
-#     def __init__(self, message: str, errors: Exception = None):
-#         super().__init__(message)
-#         self.errors = errors
 
-#     def __str__(self):
-#         if self.errors:
-#             return f"{self.args[0]} (Caused by {repr(self.errors)})"
-#         return self.args[0]
+def get_error_details(error, error_detail:sys):
+    """
+    Formats a detailed error message.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error in script '{file_name}' at line {line_number}: {str(error)}"
+    return error_message
+
+
+class CustomException(Exception):
+    """
+    Custom exception class that generates a detailed, formatted error message.
+    """
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_details = get_error_details(
+            error=error_message,
+            error_detail=error_detail
+        )
+
+    def __str__(self):
+        return self.error_details
