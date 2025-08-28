294e02fa3d4231c737eb63edcd4c75f4071ee358
matinhosseinii <matinhosseini8102@gmail.com>
2025-08-28 10:39:37 +0330

    use data_ingestion_sandbox.ipynb to experiment how to download dataset from kaggle,then add a function to download dataset from kaggle in data_ingestion.py according to the sandbox experiment, and then use the function in 01_data_ingestion.ipynb to test and download the dataset we need.

--- src/StudentsPerformance/components/data_ingestion.py
+++ src/StudentsPerformance/components/data_ingestion.py
@@ -1,1 +1,14 @@
-# This file handles the ingestion of raw data from various sources for the ML pipeline.
+import kaggle
+from StudentsPerformance.logginig.logger import setup_logger
+
+logger = setup_logger()
+
+# This function correctly uses the 'path' argument as a destination folder
+def download_kaggle_dataset(dataset_reference, path_to_save):
+    kaggle.api.dataset_download_files(
+        dataset = dataset_reference,
+        path = path_to_save,
+        unzip = True  # Automatically unzips the files for you
+    )
+
+    logger.info(f"Dataset successfully downloaded and unzipped to: src\\data\\01_Raw")
